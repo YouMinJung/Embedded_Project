80fe261f3af9036f532821d05cd63b7de4d0addb
Minjung_You <zpstls1357@naver.com>
2018-11-16 09:11:39 +0900

    Add files via upload

---
+++ Arduino/final_code1.ino
@@ -0,0 +1,47 @@
+
+#define Triger 2
+#define Echo 4
+#define BUZZER 11
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(Triger, OUTPUT);
+  pinMode(Echo, INPUT);
+  
+  pinMode(BUZZER, OUTPUT); 
+}
+
+void loop() {
+  // pulse 10us create
+  digitalWrite(Triger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(Triger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Triger, LOW);
+
+  long duration = pulseIn(Echo, HIGH);
+  long distance = duration/29/2;
+
+  Serial.print(distance);
+  Serial.print("cm");
+  Serial.println();
+
+  if(distance <= 5) {
+    tone(BUZZER, 349); // pa
+  }
+  else if(5 < distance && distance <= 10) {
+    tone(BUZZER, 330); // mi
+  }
+  else if(10 < distance && distance <= 15) {
+    tone(BUZZER, 294); // re
+  }
+  else if(15 < distance && distance <= 20) {
+    tone(BUZZER, 262); // do
+  }
+  else if(distance > 20){
+    noTone(BUZZER);
+  }
+  
+  delay(1000);
+}
---
+++ Arduino/final_code2.ino
@@ -0,0 +1,97 @@
+#include <SoftwareSerial.h>
+#include <Servo.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+#define Triger 2
+#define Echo 4
+#define BUZZER 11
+
+// set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+Servo servo;
+int value = 0; //servo motor degree
+
+int blueTx = 6;   //Tx 
+int blueRx = 5;   //Rx
+SoftwareSerial mySerial(blueTx, blueRx);  
+String myString=""; // receive string
+ 
+void setup() {
+  Serial.begin(9600);   
+  mySerial.begin(9600);
+  pinMode(13, OUTPUT);  //pin13 = LED
+
+  // servo motor
+  servo.attach(7);
+
+  // SR-04
+  pinMode(Triger, OUTPUT);
+  pinMode(Echo, INPUT);
+
+  // buzzer
+  pinMode(BUZZER, OUTPUT); 
+
+  // LCD
+  lcd.init(); // initialize the lcd
+  lcd.backlight();
+  lcd.print("Hello My Car");
+}
+ 
+void loop() {
+  // pulse 10us create
+  digitalWrite(Triger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(Triger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Triger, LOW);
+
+  long duration = pulseIn(Echo, HIGH);
+  long distance = duration/29/2;
+  char buf[20];
+  snprintf(buf, sizeof(buf), "Distance %4d cm", distance);
+  
+  lcd.setCursor(0, 2);
+  lcd.print(buf);
+
+  // buzzer ring
+  if(distance <= 4) {
+    tone(BUZZER, 700); 
+  }
+  else if(4 < distance && distance <= 8) {
+    tone(BUZZER, 400); 
+  }
+  else if(8 < distance) {
+    noTone(BUZZER);
+  }
+
+  // bluetooth module
+  while(mySerial.available()) {
+    char myChar = (char)mySerial.read();  // int -> string
+    myString += myChar;  
+    delay(5);       
+  }
+  
+  if(!myString.equals(""))  {
+    Serial.println("input value: "+myString); //시리얼모니터에 myString값 출력
+
+    // LED on/off
+    if(myString=="on") {
+        digitalWrite(13, HIGH); //LED ON
+    } 
+    else if(myString=="off") {
+        digitalWrite(13, LOW);  //LED OFF
+    }
+    // servo motor move
+    else if(myString=="forward") {
+        servo.write(0);
+    }
+    else if(myString=="backward") {
+        servo.write(180);
+    }
+      
+    myString="";  //myString 변수값 초기화
+  }
+  delay(1000);
+}
